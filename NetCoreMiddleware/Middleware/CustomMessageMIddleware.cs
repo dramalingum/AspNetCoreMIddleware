8f75017c3da13b944034a39c0785bb2b6dae3a1c
dramalingum <dramalingum@googlemail.com>
2018-07-26 00:51:50 +0100

    initial upload

---
+++ NetCoreMiddleware/Middleware/CustomMessageMIddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMiddleware.Middleware
+{
+    public class CustomMessageMiddleware
+    {
+        private RequestDelegate _next;
+
+        public CustomMessageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next.Invoke(context);
+            context.Response.WriteAsync("... my custom message here ...");
+        }
+    }
+
+}
---
+++ NetCoreMiddleware/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMiddleware.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMessage(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomMessageMiddleware>();
+        }
+
+    }
+}
